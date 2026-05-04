1cd5ae467a387f416d941a391f82c6d179807b95
albert-dev <al96@hotmail.es>
2026-05-04 12:57:41 +0200

    fix: SOLVED basics-refactor.test.ts(Repte5)

--- src/basics-refactor.test.ts
+++ src/basics-refactor.test.ts
@@ -106,33 +106,38 @@ if (last) {
    });
  });
 
-// /*
-// Repte 5:
-// Consulta la [documentació de TypeScript](https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html) i determina com canviar defaultUser perquè el test passi.
-// */
+ /*
+Repte 5:
+ Consulta la [documentació de TypeScript](https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html) i determina com canviar defaultUser perquè el test passi.
+ */
 
-// describe("Problema d'assignació de tipus a variables", () => {
-//   interface User {
-//     id: number;
-//     firstName: string;
-//     lastName: string;
-//     isAdmin: boolean;
-//   }
+ describe("Problema d'assignació de tipus a variables", () => {
+   interface User {
+     id: number;
+     firstName: string;
+     lastName: string;
+     isAdmin: boolean;
+   }
 
-//   /**
+   /**
 //    * Com ens assegurem que defaultUser sigui de tipus User
 //    * EN AQUESTA LÍNIA - no més endavant al codi?
 //    */
-//   const defaultUser = {};
+   const defaultUser = {
+    id: 1,
+    firstName: "Albert",
+    lastName: "Muntal",
+    isAdmin: true
+   };
 
-//   const getUserId = (user: User) => {
-//     return user.id;
-//   };
+   const getUserId = (user: User) => {
+     return user.id;
+   };
 
-//   it("Ha d'obtenir l'identificador de l'usuari", () => {
-//     expect(getUserId(defaultUser)).toEqual(1);
-//   });
-// });
+   it("Ha d'obtenir l'identificador de l'usuari", () => {
+     expect(getUserId(defaultUser)).toEqual(1);
+   });
+ });
 
 // /*
 // Repte 6:
