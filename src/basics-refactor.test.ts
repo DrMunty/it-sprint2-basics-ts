af7b88af9f548033fef89c2782f9664d35b9aa6f
albert-dev <al96@hotmail.es>
2026-05-04 12:51:00 +0200

    fix: SOLVED basics-refactor.test.ts(Repte3)

--- src/basics-refactor.test.ts
+++ src/basics-refactor.test.ts
@@ -54,31 +54,31 @@ describe("Problema de paràmetre objecte", () => {
 // Has d'esbrinar com tipar l'objecte perquè 'last' sigui opcional.
 // */
 
-// describe("Problema de propietats opcionals", () => {
-//   const getName = (params: { first: string; last: string }) => {
-//     if (params.last) {
-//       return `${params.first} ${params.last}`;
-//     }
-//     return params.first;
-//   };
+describe("Problema de propietats opcionals", () => {
+const getName = (params: { first: string; last?: string }) => {
+ if (params.last) {
+return `${params.first} ${params.last}`;
+     }
+     return params.first;
+  };
 
-//   it("Ha de funcionar només amb el nom", () => {
-//     const name = getName({
-//       first: "Jen",
-//     });
+ it("Ha de funcionar només amb el nom", () => {
+    const name = getName({
+       first: "Jen",
+     });
 
-//     expect(name).toEqual("Jen");
-//   });
+    expect(name).toEqual("Jen");
+  });
 
-//   it("Ha de funcionar amb el nom i el cognom", () => {
-//     const name = getName({
-//       first: "Jen",
-//       last: "Simmons",
-//     });
+   it("Ha de funcionar amb el nom i el cognom", () => {
+     const name = getName({
+      first: "Jen",
+       last: "Simmons",
+    });
 
-//     expect(name).toEqual("Jen Simmons");
-//   });
-// });
+     expect(name).toEqual("Jen Simmons");
+   });
+ });
 
 // /*
 // Repte 4:
