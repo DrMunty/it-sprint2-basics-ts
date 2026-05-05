de76d04b3e0969fdec167672dd94af44a2292aba
albert-dev <al96@hotmail.es>
2026-05-05 13:24:18 +0200

    fix: SOLVED transformation-types.test.ts(Repte-3)

--- src/transformation-types.test.ts
+++ src/transformation-types.test.ts
@@ -50,25 +50,25 @@ import { Equal, Expect } from "./helpers/type-utils";
    ];
  });
 
-// /*
-// Repte 3:
-// Utilitza ReturnType per obtenir el tipus de retorn de getUser.
-// */
-// describe("Transformació: obtenir el tipus de retorn d'una funció asíncrona", () => {
-//   const getUser = () => {
-//     return Promise.resolve({
-//       id: "123",
-//       name: "John",
-//       email: "john@example.com",
-//     });
-//   };
+ /*
+ Repte 3:
+ Utilitza ReturnType per obtenir el tipus de retorn de getUser.
+ */
+ describe("Transformació: obtenir el tipus de retorn d'una funció asíncrona", () => {
+   const getUser = ()  => {
+     return Promise.resolve({
+       id: "123",
+       name: "John",
+       email: "john@example.com",
+     });
+   };
 
-//   type ReturnValue = ReturnType<typeof getUser>;
+   type ReturnValue = Awaited<ReturnType <typeof getUser>>;
 
-//   type tests = [
-//     Expect<Equal<ReturnValue, { id: string; name: string; email: string }>>,
-//   ];
-// });
+   type tests = [
+     Expect<Equal<ReturnValue, { id: string; name: string; email: string }>>,
+   ];
+ });
 
 // /*
 // Repte 4:
