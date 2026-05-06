98ca4db67d9625658dde5d63d1d8d2ed0236118b
albert-dev <al96@hotmail.es>
2026-05-06 11:43:52 +0200

    fix: SOLVED transformation-types.test.ts(Repte-10)

--- src/transformation-types.test.ts
+++ src/transformation-types.test.ts
@@ -222,29 +222,29 @@ import { Equal, Expect } from "./helpers/type-utils";
    }
  });
 
-// /*
-// Repte 10:
-// Extreu un tipus d'un element concret d'una discriminated union.
-// */
-// describe("Transformació: extract d'una discriminated union", () => {
-//   type Event =
-//     | {
-//         type: "click";
-//         event: MouseEvent;
-//       }
-//     | {
-//         type: "focus";
-//         event: FocusEvent;
-//       }
-//     | {
-//         type: "keydown";
-//         event: KeyboardEvent;
-//       };
+ /*
+ Repte 10:
+ Extreu un tipus d'un element concret d'una discriminated union.
+ */
+ describe("Transformació: extract d'una discriminated union", () => {
+   type Event =
+     | {
+         type: "click";
+         event: MouseEvent;
+       }
+     | {
+         type: "focus";
+         event: FocusEvent;
+       }
+     | {
+         type: "keydown";
+         event: KeyboardEvent;
+       };
 
-//   type ClickEvent = unknown;
+   type ClickEvent = Extract <Event, {type: "click"}>;
 
-//   type tests = [Expect<Equal<ClickEvent, { type: "click"; event: MouseEvent }>>];
-// });
+   type tests = [Expect<Equal<ClickEvent, { type: "click"; event: MouseEvent }>>];
+ });
 
 // /*
 // Repte 11:
