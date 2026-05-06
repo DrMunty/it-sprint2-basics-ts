32ed9970bb77c8c1604e122a1edb963e65639d6f
albert-dev <al96@hotmail.es>
2026-05-06 11:50:57 +0200

    fix: SOLVED transformation-types.test.ts(Repte-11)

--- src/transformation-types.test.ts
+++ src/transformation-types.test.ts
@@ -246,37 +246,37 @@ import { Equal, Expect } from "./helpers/type-utils";
    type tests = [Expect<Equal<ClickEvent, { type: "click"; event: MouseEvent }>>];
  });
 
-// /*
-// Repte 11:
-// Exclou un tipus concret d'una discriminated union.
-// */
-// describe("Transformació: exclude d'una discriminated union", () => {
-//   type Event =
-//     | {
-//         type: "click";
-//         event: MouseEvent;
-//       }
-//     | {
-//         type: "focus";
-//         event: FocusEvent;
-//       }
-//     | {
-//         type: "keydown";
-//         event: KeyboardEvent;
-//       };
+ /*
+ Repte 11:
+ Exclou un tipus concret d'una discriminated union.
+ */
+ describe("Transformació: exclude d'una discriminated union", () => {
+   type Event =
+     | {
+         type: "click";
+         event: MouseEvent;
+       }
+     | {
+         type: "focus";
+         event: FocusEvent;
+       }
+     | {
+         type: "keydown";
+         event: KeyboardEvent;
+       };
 
-//   type NonKeyDownEvents = unknown;
+   type NonKeyDownEvents = Exclude<Event, {type: "keydown"}>;
 
-//   type tests = [
-//     Expect<
-//       Equal<
-//         NonKeyDownEvents,
-//         | { type: "click"; event: MouseEvent }
-//         | { type: "focus"; event: FocusEvent }
-//       >
-//     >,
-//   ];
-// });
+   type tests = [
+     Expect<
+       Equal<
+         NonKeyDownEvents,
+         | { type: "click"; event: MouseEvent }
+         | { type: "focus"; event: FocusEvent }
+       >
+     >,
+   ];
+ });
 
 // /*
 // Repte 12:
